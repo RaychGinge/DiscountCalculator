bc3c89e6cd0cad18d6e746c79a8e4ca3a8e043a8
Rachel Bourne <60284593+RaychGinge@users.noreply.github.com>
2021-08-12 21:13:49 +0100

    Add files

---
+++ app/src/main/java/com/example/discountcalculator/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.discountcalculator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.discountcalculator.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener{ calculateDiscount()}
+    }
+
+    private fun calculateDiscount() {
+        val stringValue1 = binding.costOfItem.text.toString()
+        val stringValue2 = binding.otherInput.text.toString()
+        val cost = stringValue1.toDoubleOrNull()
+        val otherDiscount = stringValue2.toDoubleOrNull()
+        if (cost == null) {
+            binding.discountResult.text = ""
+            return
+        }
+        val discountPercentage = when (binding.discountOptions.checkedRadioButtonId) {
+            R.id.ten_percent -> 0.10
+            R.id.twenty_percent -> 0.20
+            R.id.fifty_percent -> 0.50
+            R.id.other -> otherDiscount?.div(100)
+            else -> return
+
+        }
+        var discount = discountPercentage?.times(cost)
+
+        NumberFormat.getCurrencyInstance()
+        val formattedDiscount = NumberFormat.getCurrencyInstance().format(discount)
+        binding.discountResult.text = getString(R.string.discount_amount, formattedDiscount)
+
+
+    }
+}
